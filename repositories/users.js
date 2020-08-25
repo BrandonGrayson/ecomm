b0b2b6350bd1304dc4ef2544407b9298e1d8644a
Brandon Grayson <brandonbgrayson@gmail.com>
2020-08-25 12:27:49 -0400

    Create function now stores the contents of returned by getAll

--- repositories/users.js
+++ repositories/users.js
@@ -23,11 +23,22 @@ class usersRepository  {
             encoding: 'utf-8'
         }));
     }
+
+    // create function 
+    async create(attrs) {
+        // create a variable that will store the contents of the get all function
+        const records = await this.getAll();
+        records.push(attrs);
+        // write the updated "records" array back to this.filename
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }   
 
 // Set up an asynchronos test functiion to run repo and create a new users repository
 const test = async () => {
     const repo = new usersRepository('users.json');
+
+   await repo.create({ email:'test@test.com', password: 'password'});
 
     const users =  await repo.getAll();
     console.log(users);
